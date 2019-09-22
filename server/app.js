97fc339cb5ce1b11ff32a09fae5bcefcc732d620
chegde <chegde@foxmail.com>
2019-09-22 22:57:43 +0800

    chore: continue

--- server/app.js
+++ server/app.js
@@ -6,13 +6,28 @@ const fs = require('fs')
 app.use(express.json({ limit: '50mb' })) // for parsing application/json
 app.use(express.urlencoded({ extended: true, limit: '50mb' })) // for parsing application/x-www-form-urlencoded
 
+const basePath = 'I:/iphoneBackup/auto/';
+
+app.get('/photo', (req, res) => {
+    let existCount = 0;
+    let existFileName = basePath + existCount + '.jpeg';
+    while (fs.existsSync(existFileName)) {
+        existCount = existCount + 1;
+        existFileName = basePath + existCount + '.jpeg';
+    }
+
+    res.status(200).send(`${existCount}`);
+})
+
+
 app.post('/photo', (req, res) => {
     const { data, name } = req.body;
 
-    fs.writeFile('I:/iphoneBackup/auto/' + name + '.jpeg', Buffer.from(data, 'base64'), () => {
+    const pathFileName = basePath + name + '.jpeg';
+
+    fs.writeFile(pathFileName, Buffer.from(data, 'base64'), () => {
         res.send('ok');
     })
-
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Backup photo app listening on port ${port}!`))
